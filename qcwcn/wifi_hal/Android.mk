9c76e501f3849ac2328670b9aa62635b061cefe4
Dan Willemsen <dwillemsen@google.com>
2020-01-27 19:33:51 -0800

    Stop using LOCAL_COPY_HEADERS
    All the users of this header are already using it as if
    LOCAL_EXPORT_C_INCLUDE_DIRS has been set.
    
    Bug: 130719878
    Test: m libwifi-hal-qcom
    Test: treehugger
    Change-Id: I3c335b016208bfa5064f4a02f9f16447c6d29351

--- qcwcn/wifi_hal/Android.mk
+++ qcwcn/wifi_hal/Android.mk
@@ -56,8 +56,7 @@ LOCAL_C_INCLUDES += \
 	external/libnl/include \
 	$(call include-path-for, libhardware_legacy)/hardware_legacy \
 	external/wpa_supplicant_8/src/drivers \
-	$(TARGET_OUT_HEADERS)/libwpa_client \
-	$(TARGET_OUT_HEADERS)/cld80211-lib
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
 
 LOCAL_SRC_FILES := \
 	list.cpp \
@@ -126,8 +125,7 @@ LOCAL_C_INCLUDES += \
 	external/libnl/include \
 	$(call include-path-for, libhardware_legacy)/hardware_legacy \
 	external/wpa_supplicant_8/src/drivers \
-	$(TARGET_OUT_HEADERS)/libwpa_client \
-	$(TARGET_OUT_HEADERS)/cld80211-lib
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
 
 LOCAL_SRC_FILES := \
 	list.cpp \
